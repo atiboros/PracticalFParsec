63de6418d579940b9a80157519c4cedd7d217ec3
Attila Boros <aboros@inscopeinternational.com>
2015-10-12 12:13:55 -0400

    Initial setup

---
+++ PracticalFParsec/Chaining.fs
@@ -0,0 +1,25 @@
+﻿module Chaining
+
+open FParsec
+
+
+type Parser<'t> = Parser<'t, unit>
+
+
+let p1:Parser<_> = pstringCI "Jan"
+
+let p2:Parser<_> = pint8
+
+let pMonth:Parser<_> = p1 .>> p2
+
+let pDay:Parser<_> = p1 >>. p2
+
+let pMonthAndDay:Parser<_> = p1 .>>. p2
+
+let run1 p s = 
+    match runParserOnString p () "" s with
+    | Success(result, _, _) -> result
+    | Failure(errorMessage, _, _) -> fail errorMessage
+     
+
+run1 pMonth "Jan 1"
