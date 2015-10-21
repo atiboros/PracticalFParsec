f1cca3efb65b387c18a5b990a540049320a8850c
Attila Boros <aboros@inscopeinternational.com>
2015-10-21 13:46:57 -0400

    more backtracking

--- PracticalFParsec/Choice.fs
+++ PracticalFParsec/Choice.fs
@@ -3,7 +3,6 @@
 open FParsec
 
 
-//type P<'t> = Parser<'t, Unit>
 
 let pMonth =
     pstringCI "JAN"  <|> pstringCI "FEB"  <|> pstringCI "MAR"  <|>
@@ -11,13 +10,31 @@ let pMonth =
     pstringCI "JUL"  <|> pstringCI "AUG"  <|> pstringCI "SEP"  <|>
     pstringCI "OCT"  <|> pstringCI "NOV"  <|> pstringCI "DEC"
 
+let (<||>) p1 p2 =
+    p1 <|> (attempt p2)
+
 let run p s = 
     let unit = ()
     let result = runParserOnString p unit "" s
     match result with
     | Success(result, _, _) -> result
     | Failure(errorMessage, _, _) -> failwith errorMessage
-     
+
 
 run pMonth "Feb" 
+
+type P<'t> = Parser<'t, Unit>
+
+let pMonthAndDay:P<_> =
+    let md monthname = pstringCI monthname .>>. (spaces >>. pint8)
+    md "JAN"  <||> md "FEB"  <||> md "MAR"  <||>
+    md "APR"  <||> md "MAY"  <||> md "JUN"  <||>
+    md "JUL"  <||> md "AUG"  <||> md "SEP"  <||>
+    md "OCT"  <||> md "NOV"  <||> md "DEC"
+
+     
+
+
+
+run pMonthAndDay "Mar 21"
 
